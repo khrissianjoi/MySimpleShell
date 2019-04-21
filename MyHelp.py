4218412b6dfce4d71141fe8160d98e8996aa5e2c
Khrissian Joi Neuda <34526517+khrissianjoi@users.noreply.github.com>
2019-04-21 01:13:43 +0100

    Update MyHelp.py

--- MyHelp.py
+++ MyHelp.py
@@ -1,11 +1,12 @@
 help_dic = {
-	'cd' : 'print(RED + "CD <directory>" + RED + WHITE + " - cd command changes the current default directory to <directory>. If the <directory> argument is not present, report the current directory. If the directory does not exist an appropriate error should be reported." + WHITE)',
-	'echo' : 'print(RED+ "ECHO <comment>" +RED + WHITE +" - echo command displays <comment> on the display followed by a new line"+WHITE)',
-	'environ' : 'print(RED+"ENVIRON"+ RED + WHITE + " - environ command list all the environment strings" + WHITE)',
-	'clr' : 'print(RED + "CLR" + RED + WHITE + " - clears the shell" + WHITE)',
-	'quit': 'print(RED + "QUIT" + RED + WHITE + " - quit command raises the SystemExist and stops the execution of this script." + WHITE)',
-	'pause' : 'print(RED + "PAUSE" + RED + WHITE + " - pause command suspends the operation of the shell until Enter is pressed by the user." + WHITE)',
-	'dir' : 'print(RED + "DIR <directory>" + RED + WHITE + " - dir command lists the contents of directory <directory>." + WHITE)'
+	'cd' : ('print(RED + "CD <directory>" + RED + WHITE + " - cd command changes the current default directory to <directory>. If the <directory> argument is not present, report the current directory. If the directory does not exist an appropriate error should be reported." + WHITE)', 'cd - Accepts argument (eg. cd <directory>). Changes the current directory to supplied directory argument/<directory> by the user.'),
+	'echo' : ('print(RED+ "ECHO <comment>" +RED + WHITE +" - echo command displays <comment> on the display followed by a new line"+WHITE)','echo - Accepts arguments (eg. echo <comment>). Displays the <comment> supplied by the user.'),
+	'environ' : ('print(RED+"ENVIRON"+ RED + WHITE + " - environ command list all the environment strings" + WHITE)','environ - lists the environment strings'),
+	'clr' : ('print(RED + "CLR" + RED + WHITE + " - clears the shell" + WHITE)', 'clr - Clears the shell screen'),
+	'quit': ('print(RED + "QUIT" + RED + WHITE + " - quit command raises the SystemExist and stops the execution of this script." + WHITE)','quit - Quits the shell'),
+	'pause' : ('print(RED + "PAUSE" + RED + WHITE + " - Pause command suspends the operation of the shell until ENTER is pressed by the user." + WHITE)', 'pause -  Pause the operation of the shell, until ENTER is entered by the user.'),
+	'dir' : ('print(RED + "DIR <directory>" + RED + WHITE + " - dir command lists the contents of directory <directory>.")',  'dir - Lists the contents of the current directory'),
+	'help' : ('print(RED + "HELP" +RED + WHITE + " - Accepts arugment (eg. help <command> / help(<command>)). If argument is supplied the description of that command is displayed. Otherwise, it lists all the commands, one by one (by pressing Enter). + WHITE")','help - Accepts arugment (eg. help <command> / help(<command>)). If argument is supplied the description of that command is displayed. Otherwise, it lists all the commands, one by one (by pressing Enter).')
 }
 
 WHITE = '\033[37;0m'
@@ -13,25 +14,38 @@ RED = '\033[31;1m'
 CYAN = '\033[36;1m'
 BOLD = '\033[37;1m'
 
-class MyShellHelp():
+class MyHelp():
+	"""Help command, called by MyShell class when user calls help <command> or help(<command>), the class then uses the"""
+	"""help_dic dictionary for an output for the appropriate help command description needed""" 
 	@staticmethod
 	def help_cd():
-		eval(help_dic['cd'])
+		command = help_dic['cd'][0]
+		eval(command)
 
 	def help_echo():
-		eval(help_dic['echo'])
+		command = help_dic['echo'][0]
+		eval(command)
 
 	def help_environ():
-		eval(help_dic['environ'])
+		command = help_dic['environ'][0]
+		eval(command)
 
 	def help_clr():
-		eval(help_dic['clr'])
+		command = help_dic['clr'][0]
+		eval(command)
 
 	def help_quit():
-		eval(help_dic['quit'])
+		command = help_dic['quit'][0]
+		eval(command)
 
 	def help_pause():
-		eval(help_dic['pause'])
+		command =help_dic['pause'][0]
+		eval(command)
 		
 	def help_dir():
-		eval(help_dic['dir'])
+		command = help_dic['dir'][0]
+		eval(command)
+
+	def help_help():
+		command = help_dic['help'][0]
+		eval(command)
